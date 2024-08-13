36dcf7f7fbbb27f0ccc73a6dc3584d87c089fb2d
DanielCar99 <danielcar2680@gmail.com>
2024-08-13 09:02:48 -0500

    Codigo de prueba Peter Corke

---
+++ peter_corke0.py
@@ -0,0 +1,27 @@
+import sys
+import io
+from roboticstoolbox import *
+import math
+
+# Establecer la codificación a utf-8
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+l1 = 12
+l2 = 14
+l3 = 6
+l4 = 4
+
+q1 = 0
+q2 = 0
+
+R = []
+R.append(RevoluteDH(d=l1, alpha=math.pi/2, a=l2, offset=0))
+R.append(RevoluteDH(d=l3, alpha=0, a=l4, offset=0))
+
+Robot = DHRobot(R, name='Bender')
+print(Robot)
+
+Robot.teach([q1, q2], limits=[-30,30,-30,30,-30,30])
+
+MTH = Robot.fkine([q1, q2])
+print(MTH)
